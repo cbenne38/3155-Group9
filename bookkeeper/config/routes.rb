e33b70b7b6ff330e4c0b960cf82a40ee142e4c01
Chad Bennett <cbenne38@uncc.edu>
2020-03-18 19:40:37 +0000

    Added Library page and set routes

--- bookkeeper/config/routes.rb
+++ bookkeeper/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'library#index'
+  get 'library/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
