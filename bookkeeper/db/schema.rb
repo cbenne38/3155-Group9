cb9388eaa8eb81e931b890562ea4f113b29ddcb4
Chad Bennett <cbenne38@uncc.edu>
2020-04-01 17:14:49 +0000

    Added book model and migrated

--- bookkeeper/db/schema.rb
+++ bookkeeper/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_170624) do
+ActiveRecord::Schema.define(version: 2020_04_01_171301) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "author"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
