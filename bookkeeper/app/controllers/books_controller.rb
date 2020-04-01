aa3a557dfc52318b48298b35bad70ec47df0f9d8
Vanessa Liaw <vliaw@uncc.edu>
2020-04-01 17:21:55 +0000

    Created migration for AddUsersToBooks

--- bookkeeper/app/controllers/books_controller.rb
+++ bookkeeper/app/controllers/books_controller.rb
@@ -1,2 +1,8 @@
 class BooksController < ApplicationController
+    def new 
+    end
+    
+    def create
+        @book.user = current_user
+    end
 end
--- bookkeeper/config/routes.rb
+++ bookkeeper/config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
+  resources :books
+  
   root 'library#index'
   get 'library/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
